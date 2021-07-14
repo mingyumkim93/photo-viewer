5e35c2d1d5e1a6aa55587111d24eba33d229f847
MingyumKim <Mingyum93@gmail.com>
2021-07-14 12:10:21 +0300

    database test

--- api/medias.ts
+++ api/medias.ts
@@ -3,8 +3,10 @@ import { connectToDatabase } from "../src/lib/database";
 
 export default async function medias(req: VercelRequest, res: VercelResponse) {
   const db = await connectToDatabase();
-  const collection = await db.collection("medias");
+  console.log("db:", db);
+  const collection = db.collection("medias");
+  console.log("collection:", collection);
   const medias = await collection.find({}).toArray();
-
+  console.log("medias:", medias);
   res.status(200).json({ medias });
 }
